a0ee87c91c0558d43d9489a0aa38013ae42b12ed
Jamal Winchester <jamalwinchester@gmail.com>
2017-01-31 06:36:19 -0400

    First Commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/OneOptics/admin/');
+define('HTTP_CATALOG', 'http://localhost:8080/OneOptics/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/OneOptics/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8080/OneOptics/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/OneOptics/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/OneOptics/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/OneOptics/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/OneOptics/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/OneOptics/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/OneOptics/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/OneOptics/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/OneOptics/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/OneOptics/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/OneOptics/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/OneOptics/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/OneOptics/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'oneoptics');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/OneOptics/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/OneOptics/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/OneOptics/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/OneOptics/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/OneOptics/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/OneOptics/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/OneOptics/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/OneOptics/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/OneOptics/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/OneOptics/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/OneOptics/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/OneOptics/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/OneOptics/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'oneoptics');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
